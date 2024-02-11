9e2bba3d7344a72ffbbb3afc1f27df296e1f25a3
Javedmv <javedmv777@gmail.com>
2024-02-11 20:20:18 +0530

    added the winning condition

--- script.js
+++ script.js
@@ -31,26 +31,39 @@ score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
 const scores = [0,0];
+
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 btnRoll.addEventListener('click',function(){
-    const dice = Math.trunc(Math.random() * 6)+1;
-    diceEl.classList.remove("hidden");
-    diceEl.src = `dice-${dice}.png`;
-
-    if(dice !== 1){
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-
-    }else{
-        switchPlayer();
+    if(playing){
+        const dice = Math.trunc(Math.random() * 6)+1;
+        diceEl.classList.remove("hidden");
+        diceEl.src = `dice-${dice}.png`;
+        
+        if(dice !== 1){
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+            
+        }else{
+            switchPlayer();
+        }
     }
 })
 
 btnHold.addEventListener('click',function(){
-    scores[activePlayer] += currentScore;
-    
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer ];
-
+    if(playing){
+        scores[activePlayer] += currentScore;
+        
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer ];
+        
+        if(scores[activePlayer] >= 100){
+            playing = false;
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        }else{
+            switchPlayer();
+        }
+    }
 })
