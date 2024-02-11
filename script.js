8261eb01cc5c978b47829e4b7c709284567fbcc4
Javedmv <javedmv777@gmail.com>
2024-02-11 20:00:17 +0530

    completed switching the player

--- script.js
+++ script.js
@@ -11,13 +11,18 @@ const btnHold = document.querySelector(".btn--hold");
 const current0El = document.getElementById("current--0")
 const current1El = document.getElementById("current--1")
 
+const player0El = document.querySelector(".player--0")
+const player1El = document.querySelector(".player--1")
+
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 
 diceEl.classList.add("hidden");
 
+const scores = [0,0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click',function(){
     const dice = Math.trunc(Math.random() * 6)+1;
@@ -26,6 +31,16 @@ btnRoll.addEventListener('click',function(){
 
     if(dice !== 1){
         currentScore += dice;
-        current0El.textContent = currentScore;
-    } 
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
+    }else{
+        //switch player
+        activePlayer = activePlayer === 0? 1 : 0;
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        
+        //toggle will remove the class if it exsist else add
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
+    }
 })
