ec26909c834ced47840fa02942709a4001125cb1
Javedmv <javedmv777@gmail.com>
2024-02-11 20:36:23 +0530

    project completed

--- script.js
+++ script.js
@@ -14,6 +14,28 @@ const current1El = document.getElementById("current--1")
 const player0El = document.querySelector(".player--0")
 const player1El = document.querySelector(".player--1")
 
+let currentScore, activePlayer, playing, scores
+
+const init = function(){
+    scores = [0,0];    
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add("hidden");
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--winner");
+    player1El.classList.remove("player--winner");
+};
+
+init();
+
 const switchPlayer = function(){
             //switch player
             document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -23,18 +45,8 @@ const switchPlayer = function(){
             //toggle will remove the class if it exsist else add
             player0El.classList.toggle("player--active");
             player1El.classList.toggle("player--active");
-}
+};
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-
-diceEl.classList.add("hidden");
-
-const scores = [0,0];
-
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
 btnRoll.addEventListener('click',function(){
     if(playing){
@@ -50,7 +62,7 @@ btnRoll.addEventListener('click',function(){
             switchPlayer();
         }
     }
-})
+});
 
 btnHold.addEventListener('click',function(){
     if(playing){
@@ -60,10 +72,13 @@ btnHold.addEventListener('click',function(){
         
         if(scores[activePlayer] >= 100){
             playing = false;
+            diceEl.classList.add("hidden");
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
         }else{
             switchPlayer();
         }
     }
-})
+});
+
+btnNew.addEventListener("click",init);
